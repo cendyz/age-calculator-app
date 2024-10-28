5cfe67bcbd1167546a37fe614a6c6ee69ad3de97
cendyz <arek57823@gmail.com>
2024-10-28 09:27:04 +0100

    java already done

--- src/main.js
+++ src/main.js
@@ -2,47 +2,63 @@ const dayInput = document.getElementById("day");
 const monthInput = document.getElementById("month");
 const yearInput = document.getElementById("year");
 const inputs = [dayInput, monthInput, yearInput];
+const finalBirthInfo = document.querySelectorAll(".main__content-text span");
 const btn = document.getElementById("btn");
 const regexLetters = /^[a-zA-Z]+$/;
-const date = new Date()
-const day = new Date().getDate()
-const month = date.getMonth() + 1
-const year = date.getFullYear()
+const currentDate = new Date();
+const currentYear = currentDate.getFullYear();
+
+console.log(currentYear);
 
 const checkInputsError = input => {
 	const nameOfTheInput = input.previousElementSibling;
 	const errorText = input.nextElementSibling;
 	const numMax = parseFloat(input.max);
 	const yearErrorText = yearInput.nextElementSibling;
+	let isValid;
 
 	if (input.value === "") {
 		changeColorsToRed(input, nameOfTheInput, errorText);
 	} else if (input.value > numMax || input.value < 1) {
 		changeColorsToRed(input, nameOfTheInput, errorText);
-		errorText.textContent = "Must be a valid day";
-	} else if (yearInput.value > year) {
-		changeColorsToRed(yearInput, yearInput.previousElementSibling, yearErrorText);
-		yearErrorText.textContent = "Must be a valid day";
-	} else {
+		errorText.textContent = `Must be a valid ${nameOfTheInput.textContent}`;
+	}
+
+	// else if (yearInput.value > currentYear ) {
+	// 	changeColorsToRed(yearInput, yearInput.previousElementSibling, yearErrorText);
+	// 	yearErrorText.textContent = "Must be in the past";
+	// }
+	else {
 		changeColorsToNormal(input, nameOfTheInput, errorText);
+		// calculateBirth();
+		
 	}
 };
 
-const calculateDate = () => {
-	let myYear = year - yearInput.value
-	// let myMonth =
-}
-
 const changeColorsToRed = (input, nameOfTheInput, errorText) => {
 	nameOfTheInput.style.color = "hsl(0, 100%, 67%)";
-	input.style.border = "1px solid hsl(0, 100%, 67%)";
 	errorText.style.display = "block";
+	input.style.border = "1px solid hsl(0, 100%, 67%)";
 };
 
 const changeColorsToNormal = (input, nameOfTheInput, errorText) => {
 	nameOfTheInput.style.color = "hsl(0, 1%, 44%)";
 	input.style.border = "1px solid hsl(0, 0%, 86%)";
 	errorText.style.display = "none";
+};
+
+const calculateBirth = () => {
+	let birthDay = currentDate.getDate();
+	let birthMonth = currentDate.getMonth() + 1;
+	let birthYear = currentYear;
+
+	birthDay = birthDay - dayInput.value;
+	birthMonth = birthMonth - monthInput.value;
+	birthYear = birthYear - yearInput.value;
+
+	finalBirthInfo[2].textContent = birthDay;
+	finalBirthInfo[1].textContent = birthMonth;
+	finalBirthInfo[0].textContent = birthYear;
 };
 
 btn.addEventListener("click", () => {
@@ -55,9 +71,8 @@ inputs.forEach(input => {
 	});
 });
 
-window.onload = () => {
-	inputs.forEach(input => {
-		input.value = '';
-	});
-	
-};
+// window.onload = () => {
+// 	inputs.forEach(input => {
+// 		input.value = "";
+// 	});
+// };
